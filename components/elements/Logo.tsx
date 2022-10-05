b1bae3d46ab63f83dbc35a134bd19595313cb438
Muzammil-hasan <muzammilsyed270300@gmail.com>
2022-10-05 13:22:33 +0530

    feat: responsive header

--- components/elements/Logo.tsx
+++ components/elements/Logo.tsx
@@ -7,12 +7,8 @@ export default function Logo({ className = '' }: Props) {
   return (
     <Link href={'/'}>
       <a className={clsx('flex items-center gap-x-2', { [className]: className })}>
-        <img
-          src="/images/logo.png"
-          alt="logo"
-          className="object-contain h-auto lg:w-[7.5rem] w-14"
-        />
-        <p className="-mt-2 text-sm font-semibold lg:text-3xl">Ramaera industries</p>
+        <img src="/images/logo.png" alt="logo" className="object-contain w-12 h-auto lg:w-28" />
+        <p className="text-xs font-semibold lg:text-2xl">Ramaera industries</p>
       </a>
     </Link>
   );
--- layout/Header.tsx
+++ layout/Header.tsx
@@ -1,10 +1,16 @@
 import Logo from '@/components/elements/Logo';
 import BellIcon from '@/components/icons/BellIcon';
-import TriangleDownIcon from '@/components/icons/TriangleDownIcon';
+import { TriangleDownIcon } from '@/components/icons/TriangleDownIcon';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
+import { useState } from 'react';
+
+// TODO: no-event on header span
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
   return (
     <header className="relative flex items-center justify-between px-4 py-4 lg:px-8">
+      <span className="block w-[86%] h-0.25 ml-auto bg-white absolute lg:bottom-4 bottom-0 lg:right-10 inset-x-4" />
       <Logo />
 
       <div className="flex items-center gap-x-3 lg:gap-x-6">
@@ -12,11 +18,14 @@ export default function Header() {
           <BellIcon className="w-5 h-5 lg:w-7 lg:h-7" />
         </button>
 
-        <button className="flex items-center text-xs truncate lg:text-sm gap-x-2">
-          English &#40;UK&#41; <TriangleDownIcon className="w-2 h-2 lg:w-3.5 lg:h-3.5" />
+        <button className="items-center hidden text-sm truncate gap-x-2 lg:flex">
+          English &#40;UK&#41; <TriangleDownIcon className="w-3.5 h-3.5" />
+        </button>
+
+        <button onClick={() => setOpen((prev) => !prev)} className="lg:hidden">
+          {!open ? <Bars3Icon className="w-6 h-6" /> : <XMarkIcon className="w-6 h-6" />}
         </button>
       </div>
-      <span className="block w-[85%] h-0.5 ml-auto bg-white absolute lg:bottom-4 bottom-0 lg:right-10 inset-x-4" />
     </header>
   );
 }
